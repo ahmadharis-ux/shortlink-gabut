c0ec690551834fcad11c33daa8616e83d6f8cfac
ahmadharis-ux <ahmadfreedom27@gmail.com>
2023-06-05 21:55:54 +0700

    Update DaftarController.php, LoginController.php, and login.blade.php

---
+++ resources/views/login-register/login.blade.php
@@ -0,0 +1,14 @@
+@extends('main')
+@section('content')
+
+<div class="card mt-3">
+    <div class="card-header">
+        Login
+    </div>
+    <div class="card-body">
+        <form action="">
+            <input type="text" class="form-control">
+        </form>
+    </div>
+</div>
+@endsection
